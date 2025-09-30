80015bdcb730c66180d2bf09bd3672cdd5f8d240
YoussefKlibi <Youssef.klibi@esprit.tn>
2025-09-30 22:22:30 +0100

    Delete Integration directory
    sppression

--- Integration/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/.qt/deploy_rh_fe05bb7daf.cmake
+++
@@ -1,8 +0,0 @@
-include("C:/Users/ASUS/Desktop/Smart City/Integration/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/rh-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "C:/Users/ASUS/Desktop/Smart City/Integration/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/rh.exe"
-    GENERATE_QT_CONF
-)
