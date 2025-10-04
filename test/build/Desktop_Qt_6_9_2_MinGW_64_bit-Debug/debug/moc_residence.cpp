3ea5370cfe4c9135ebe650bdac6720d8a706de5f
LaghloughElaa <Elaa.Laghlough@esprit.tn>
2025-10-04 18:49:44 +0100

    mise à jour

--- test/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/debug/moc_residence.cpp
+++ test/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/debug/moc_residence.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../../residence.h"
+#include "../../../../residence/residence.h"
 #include <QtGui/qtextcursor.h>
 #include <QtGui/qscreen.h>
 #include <QtCore/qmetatype.h>
