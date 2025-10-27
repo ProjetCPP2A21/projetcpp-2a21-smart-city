cae428c77cff1128cc844801c8e6d1d548304bf2
Youssefklibi <Youssef.klibi@esprit.tn>
2025-10-27 17:51:23 +0100

    Travail accomplit Ajout et Suppresion

--- GEvenements/evenement.cpp
+++ GEvenements/evenement.cpp
@@ -1,9 +1,24 @@
 #include "evenement.h"
+#include "connection.h"
 #include <QString>
+#include <QSqlError>
+#include <QDebug>
 
-Evenement::Evenement(int ID, QString Nom, QString Type, int Date, int Heure, QString Lieu, int Nbr_Participants)
+Evenement::Evenement()
+{
+    ID = 0;
+    Nom = "";
+    Type = "";
+    Date = "";
+    Heure = "";
+    Lieu = "";
+    Nbr_Participants = 0;
+}
+
+Evenement::Evenement(int ID,int ID_Employe, QString Nom, QString Type, QString Date, QString Heure, QString Lieu, int Nbr_Participants)
 {
     this->ID = ID;
+    this->ID_Employe = ID_Employe;
     this->Nom = Nom;
     this->Type = Type;
     this->Date = Date;
@@ -12,19 +27,33 @@ Evenement::Evenement(int ID, QString Nom, QString Type, int Date, int Heure, QSt
     this->Nbr_Participants = Nbr_Participants;
 }
 
+Evenement::~Evenement() {}
+
+
  bool Evenement::ajouter()
 {
      QSqlQuery query;
     QString res = QString::number(ID);
-     query.prepare("insert into evenement (ID_Evenement, Nom, Type, Date, Heure, Lieu, Nbr_Participants;)""values (:ID_Evenement, :Nom, :Type, :Date, :Heure, :Lieu, :Nbr_Participants");
+     query.prepare("INSERT INTO evenement (ID_Evenement, ID_Employe, Nom, Type_Evenement, Date_Evenement, Heure, Lieu, Nbr_Participants) "
+                   "VALUES (:ID_Evenement, :ID_Employe, :Nom, :Type_Evenement, :Date_Evenement, :Heure, :Lieu, :Nbr_Participants)");
     // liaison entre les attributs de la classe et de la table en base de donnée
     query.bindValue(":ID_Evenement", ID);
+    query.bindValue(":ID_Employe", ID_Employe);
     query.bindValue(":Nom", Nom);
-    query.bindValue(":Type", Type);
-    query.bindValue(":Date", Date);
+    query.bindValue(":Type_Evenement", Type);
+    query.bindValue(":Date_Evenement", Date);
     query.bindValue(":Heure", Heure);
     query.bindValue(":Lieu", Lieu);
     query.bindValue(":Nbr_Participants", Nbr_Participants);
-    return query.exec();
 
+    return query.exec();
+}
+
+bool Evenement::supprimer(int id)
+{
+    QSqlQuery query;
+    QString res = QString::number(id);
+    query.prepare("Delete from Evenement where ID_Evenement = :id");
+    query.bindValue(":id", res);
+    return query.exec();
 }
--- GEvenements/evenement.h
+++ GEvenements/evenement.h
@@ -9,32 +9,35 @@ using namespace std;
 class Evenement
 {
     int ID;
+    int ID_Employe;
     QString Nom;
     QString Type;
-    int Date;
-    int Heure;
+    QString Date;
+    QString Heure;
     QString Lieu;
     int Nbr_Participants;
 public:
     Evenement();
-    Evenement(int ID, QString Nom, QString Type, int Date, int Heure, QString Lieu, int Nbr_Participants);
+    Evenement(int ID,int ID_Employe, QString Nom, QString Type, QString Date, QString Heure, QString Lieu, int Nbr_Participants);
     ~Evenement();
 
     //Getters
     int getID(){return ID;}
+    int getID_Employe(){return ID_Employe;}
     QString getNom(){return Nom;}
     QString getType(){return Type;}
-    int getDate(){return Date;}
-    int getHeure(){return Heure;}
+    QString getDate(){return Date;}
+    QString getHeure(){return Heure;}
     QString getLieu(){return Lieu;}
     int getNbr_Participants(){return Nbr_Participants;}
 
     // setters
     void setID(int id){ID = id;}
+    void setID_Employe(int IDE){ID_Employe = IDE;}
     void setNom(QString n){Nom = n;}
     void setType(QString T){Type = T;}
-    void setDate(int D){Date = D;}
-    void setHeure(int H){Heure = H;}
+    void setDate(QString D){Date = D;}
+    void setHeure(QString H){Heure = H;}
     void setLieu(QString L){Lieu = L;}
     void setNbr_Participants(int Nbr){Nbr_Participants = Nbr;}
 
